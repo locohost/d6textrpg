558551cf7f019bc01243c27be212edbf13dd1b49
locohost <locohost@gmail.com>
2017-09-02 15:11:18 -0400

    Trying handleHookSource

--- area.js
+++ area.js
@@ -1,14 +1,16 @@
-module['exports'] = function myService(hook) {
+var hook = require('../../resources/hook');
+
+module['exports'] = function handleHookSource(req, res) {
 
     const jwt = require('jsonwebtoken'), 
           MongoClient = require('mongodb').MongoClient;
 
     MongoClient.connect(hook.env.mongoUrl_DO, function (err, database) {
         // Verify the JWT token in header
-        var token = hook.http.getHeader('authorization');
+        var token = req.getHeader('authorization');
         if (!token) {
             ///TODO: Need to log this to database warn/error log
-            return hook.res.end('Invalid token: Token is null');
+            return res.end('Invalid token: Token is null');
         } else {
             // Strip off "Bearer[space]..." token prefix
             token = token.split(" ")[1];
@@ -18,15 +20,15 @@ module['exports'] = function myService(hook) {
                 db.collection('area').find({ 'del': false }).toArray(function (err, docs) {
                     if (err) {
                         ///TODO: LOG THIS in database!...
-                        hook.res.statusCode = 404;
-                        return hook.res.end('Error! ' + (err ? ': ' + err : ''));
+                        res.statusCode = 404;
+                        return res.end('Error! ' + (err ? ': ' + err : ''));
                     }
-                    hook.res.statusCode = 201;
-                    return hook.res.end(docs);
+                    res.statusCode = 201;
+                    return res.end(docs);
                 });
             } else {
                 ///TODO: Need to log this to database warn/error log
-                return hook.res.end('Invalid token');
+                return res.end('Invalid token');
             }
         }
 
