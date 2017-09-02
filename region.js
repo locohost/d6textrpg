df476826f52416ab99e30060482ff3230989330a
locohost <locohost@gmail.com>
2017-09-02 13:42:54 -0400

    - New regions hook.io funtion

---
+++ region.js
@@ -0,0 +1,22 @@
+module['exports'] = function myService(hook) {
+
+    const jwt = require('jsonwebtoken'), 
+          MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect(hook.env.mongoUrl_DO, function (err, database) {
+        const db = database.db('game');
+
+        console.log("Connected to MongoDb: 'game' database!");
+        console.log("Getting Regions...");
+
+        db.collection('region').find({ 'del': false }, function (err, docs) {
+            if (err) {
+                ///TODO: LOG THIS in database!...
+                hook.res.statusCode = 404;
+                return hook.res.end('Error! ' + (err ? ': ' + err : ''));
+            }
+          	hook.res.statusCode = 201;
+          	return hook.res.end(docs);
+        });
+    });
+};
